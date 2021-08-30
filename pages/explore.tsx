943ab090e9fb38b756d89a1fd0b16e1be789a870
adityacaturputra <adityacaturputra25@gmail.com>
2021-08-30 16:21:57 +0700

    Start Page

---
+++ pages/explore.tsx
@@ -0,0 +1,40 @@
+import type { NextPage } from 'next';
+import { useState } from 'react';
+
+const Explore: NextPage = () => {
+
+  return (
+    <>
+      <div className="min-h-screen bg-hero bg-center bg-cover bg-fixed">
+        <div className="flex overflow-x-scrool flex-wrap justify-around">
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>  
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>  
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>  
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>  
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>  
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>  
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>  
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>  
+          <div className="shadow-inner h-60 w-60 bg-white m-6 "></div>  
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Explore;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: () => ({
+        hero: 'url(\'/images/college-girl.jfif\')',
+      }),
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};

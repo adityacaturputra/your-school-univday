6cd565f04db2524d74273540e0d33cee9be326c8
adityacaturputra <adityacaturputra25@gmail.com>
2021-08-31 11:34:17 +0700

    make anchor

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -13,16 +13,16 @@ const Explore: NextPage = () => {
             {datas.map((data, key) => (
               // eslint-disable-next-line react/jsx-key
               <Fade right delay={50*key} key={key}>
-                <div className="inline-block bg-white m-6 rounded" style={{height: '10vh', width: '10vh'}}></div>
+                <a className="inline-block bg-white m-6 rounded" style={{height: '10vh', width: '10vh'}} href={`#${key}`}>{key}{datas.length}</a>
               </Fade>
             ))}
           </div>
-          <div className="" style={{height: '80vh', position: 'relative'}}>
+          <div style={{height: '80vh', position: 'relative'}}>
             <div style={{maxHeight:'100%', overflowY:'auto', overflowX: 'hidden'}}>
               {datas.map((data, key) => (
                 // eslint-disable-next-line react/jsx-key
                 <Fade bottom key={key}>
-                  <div className="w-auto bg-white my-6 mx-6 mb-96 rounded" style={{height:900}}>{key}{datas.length}</div>
+                  <div className="w-auto bg-white my-6 mx-6 mb-96 rounded shadow-2xl" style={{height:'72vh'}} id={`${key}`}>{key}{datas.length}</div>
                 </Fade>
               ))}
             </div>

0d9df334a1e74baec9e91b9bcb43261adba22d31
adityacaturputra <adityacaturputra25@gmail.com>
2021-09-12 15:10:32 +0700

    finish project for lepkom

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -5,11 +5,14 @@ import { University} from '../src/types/UniversityTable';
 import Logos from '../src/components/molecules/Logos';
 import Contents from '../src/components/molecules/Contents';
 import Heading from '../src/components/molecules/Heading';
+
 interface Props {
-  university: University[]
+  university: University[],
+  errorMessage: string
 }
 
-const Explore: NextPage<Props> = ({university}) => {
+const Explore: NextPage<Props> = (props) => {
+  const {university} = props;
   const [DetailUniversityIndex, setDetailUniversityIndex] = useState<number>(0);
   const [fadeAnimation, setFadeAnimation] = useState(true);
   const handleDetailUniversityIndex = (index : number) => {
@@ -18,7 +21,10 @@ const Explore: NextPage<Props> = ({university}) => {
     setDetailUniversityIndex(index);
   };
   const scrollup = (scrollableElements: HTMLCollectionOf<Element>) => {
-    if (process.browser) Array.from(scrollableElements).forEach(element => element.scrollTo({top: 0}));
+    if (process.browser) {
+      Array.from(scrollableElements)
+        .forEach(element => element.scrollTo({top: 0}));
+    }
   };
 
   useEffect(() => {
@@ -31,26 +37,45 @@ const Explore: NextPage<Props> = ({university}) => {
   return (
     <div className="min-h-screen bg-hero bg-center bg-cover bg-fixed">
       <Fade>
-        <div className="h-screen md:bg-gradient-to-b bg-gradient-to-t from-black via-transparent to-transparent flex md:flex-col flex-col-reverse">
-          <Logos data={university} currentIndex={DetailUniversityIndex} onClick={handleDetailUniversityIndex} />
-          <Fade bottom when={fadeAnimation}>
-            <Contents data={university} currentIndex={DetailUniversityIndex} />
-          </Fade>
-          <Heading animation={fadeAnimation} title={university[DetailUniversityIndex].name} />
-        </div>
+        {
+          university ? 
+            <div className="h-screen md:bg-gradient-to-b bg-gradient-to-t from-black via-transparent to-transparent flex md:flex-col flex-col-reverse">
+              <Logos data={university} currentIndex={DetailUniversityIndex} onClick={handleDetailUniversityIndex} />
+              <Fade bottom when={fadeAnimation}>
+                <Contents data={university} currentIndex={DetailUniversityIndex} />
+              </Fade>
+              <Heading animation={fadeAnimation} title={university[DetailUniversityIndex].name} />
+            </div>
+            : 
+            <div>
+              <Heading animation={fadeAnimation} title={'Gagal mendapatkan data'} />
+              <Heading animation={fadeAnimation} title={props.errorMessage} />
+            </div>
+        }
       </Fade> 
     </div>
   );
 };
 
 export async function getServerSideProps() {
-  const res = await fetch('https://admin-your-school-univday.herokuapp.com/api/v1/university/all');
-  const dataUniversity = await res.json();
-  return {
-    props : {
-      university: dataUniversity.university,
-    }
-  };
+  try {
+    const res = await fetch(
+      'https://admin-your-school-univday.herokuapp.com/api/v1/university/all'
+    );
+    const dataUniversity = await res.json();
+    return {
+      props : {
+        university: dataUniversity.university,
+      }
+    };
+  } catch (error) {
+    return {
+      props : {
+        university: null,
+        errorMessage: error.message
+      }
+    };
+  }
 }
 
 export default Explore;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,27 @@ import type { NextPage } from 'next';
 import { useRouter } from 'next/dist/client/router';
 import { useState } from 'react';
 import Button from '../src/components/atoms/Button';
-import Fade from 'react-reveal/Fade';
 import { BallBeat } from 'react-pure-loaders';
+import Fade from 'react-reveal/Fade';
 
-const Home: NextPage = () => {
+
+const Start: NextPage = () => {
   const [QtaClicked, setQtaClicked] = useState(false);
-  const router = useRouter(); 
-  const handleButtonClicked = (url : string) => {
+  const router = useRouter();
+  const handleButtonClicked = () => {
     setQtaClicked(true);
-    setTimeout(() => router.push(`${url}`), 400);
+    setTimeout(() => router.push('/explore'), 400);
   };
 
   return (
     <div className="h-screen bg-hero bg-center bg-cover">
-      <div className={'h-screen bg-opacity-80 bg-black flex flex-col justify-center'}>
+      <div className={`h-screen ${!QtaClicked && 'bg-opacity-80 bg-black'} flex flex-col justify-center transition duration-500`}>
         <Fade bottom when={!QtaClicked}>
-          <Button title="Web Utama" className="text-center flex justify-center mt-5 mx-[10vw]" onClick={() => handleButtonClicked('/start')} />
-          <Button title="Pengenalan Panitia" className="text-center flex justify-center mt-5 mx-[10vw]" onClick={() => handleButtonClicked('/intro')} />
-          <Button title="Jadwal dan Acara" className="text-center flex justify-center mt-5 mx-[10vw]" onClick={() => handleButtonClicked('/schedule')} />
-          <Button title="Contact Person" className="text-center flex justify-center mt-5 mx-[10vw]" onClick={() => handleButtonClicked('/contact')} />
+          <p className="text-center text-white text-4xl pb-10">Selamat Datang di <span className="text-purple-400">your-school-univday</span></p>
+          <p className="text-white text-xl lg:px-48 md:px-20 px-5">
+            Kami para alumni sangat peduli dengan masa depan adik adik kami, maka dengan itu kami membuat web ini sebagai sarana untuk berbagi kepada adek adek kami mengenai pengalaman kami yang sudah terjun di kampus. Semoga dengan dibuatnya web ini harapan besarnya adalah adik adik tidak kebingungan lagi akan memilih universitas mana dan akan mempertimbangkan hal tersebut secara baik baik.
+          </p>
+          <Button title="Explore your University now" className="text-center mt-10 mx-10 flex justify-center" onClick={ () => handleButtonClicked()} />
         </Fade>
         <Fade bottom when={QtaClicked}>
           <div className="text-center">
@@ -32,4 +34,4 @@ const Home: NextPage = () => {
   );
 };
 
-export default Home;
+export default Start;

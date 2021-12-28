5f5ac7c3802577fa587b18eceaecb387d4803b1e
adityacaturputra <adityacaturputra25@gmail.com>
2021-12-28 15:01:11 +0700

    logo scroll responsive on lower height

--- src/components/molecules/Logos.tsx
+++ src/components/molecules/Logos.tsx
@@ -12,14 +12,15 @@ interface Props {
 const Logos : React.FC<Props> = ({data, currentIndex, onClick,}) => {
   
   return (
-    <div className="overflow-x-scroll overflow-y-hidden whitespace-nowrap">
+    <div className="overflow-x-scroll overflow-y-hidden whitespace-nowrap z-10">
       {data.map((university: University, index: number) => (
         <Fade right delay={50*index} key={university._id} when={index !== currentIndex}>
-          <div className="inline-block bg-white m-2 sm:m-3 md:m-4 rounded cursor-pointer p-1 bg-opacity-90 h-[10vh] w-[10vh]" >
-            <Images
-              src={`/api/imageproxy?url=${encodeURIComponent(university.imageId.imageUrl)}`}
-              width="100%" height="100%" 
+          <div className="inline-block bg-white m-2 sm:m-3 md:m-4 rounded cursor-pointer p-1 bg-opacity-90 h-[8vh] w-[8vh]" >
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src={university.imageId.imageUrl}
               onClick={() => onClick(index)}
+              alt="logo-universitas"
             />
           </div>
         </Fade>

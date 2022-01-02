80af67d2b163c56e21b37dc243933193bfa728a1
adityacaturputra <adityacaturputra25@gmail.com>
2022-01-02 13:38:58 +0700

    content and logo responsive view

--- src/components/molecules/Contents.tsx
+++ src/components/molecules/Contents.tsx
@@ -10,8 +10,8 @@ interface Props {
 const Contents : React.FC<Props> = ({data, currentIndex}) => {
   
   return (
-    <div className="h-[83vh] relative">
-      <div className="flex flex-wrap justify-evenly scrollup max-h-[100%] overflow-y-auto overflow-x-hidden">
+    <div className="h-[94vh] md:h-[96vh] relative">
+      <div className="flex flex-wrap justify-evenly scrollup max-h-[100%] overflow-y-auto overflow-x-hidden md:pt-[20vh] md:pb-0 pt-0 pb-[20vh]">
         {
           data[currentIndex].contentId.map((konten: Content) => (
             <div key={konten._id} className="max-w-lg md:max-h-96 overflow-auto bg-white bg-opacity-90 my-4 mx-1 p-3 rounded scrollup">

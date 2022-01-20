533ad16eec88b96048405733c2f79fdb8b3dee14
adityacaturputra <adityacaturputra25@gmail.com>
2022-01-20 23:46:41 +0700

    style: separate the function

--- pages/schedule.tsx
+++ pages/schedule.tsx
@@ -6,29 +6,10 @@ import { useEffect, useState } from 'react';
 import ScrollableBox from '../src/components/molecules/ScrollableBox';
 import { Schedule } from '../src/types/University';
 import Heading from '../src/components/molecules/Heading';
+import formatDate from '../src/utils/dateFormatter';
 
 
-const dayList = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
-const monthList = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
 
-const getDate = (strDate : string) => {
-  const dateSplitTime = strDate.split('T');
-  const YYYYMMDD = dateSplitTime[0].split('-');
-  const year = YYYYMMDD[0];
-  const monthIndex = parseInt(YYYYMMDD[1]) - 1;
-  const month = monthList[monthIndex];
-  const dayInMonth = YYYYMMDD[2];
-  const time = dateSplitTime[1].split('.')[0].split(':')[0] + ':' + dateSplitTime[1].split('.')[0].split(':')[1];
-  const dayIndex = new Date(strDate).getDay();
-  const day = dayList[dayIndex];
-
-  return { date: {year, month, dayInMonth}, time, day };
-};
-
-const formatDate = (strDate: string) => {
-  const {date, time, day} = getDate(strDate);
-  return `${time} - ${day}, ${date.dayInMonth} ${date.month} ${date.year}`;
-};
 
 const SchedulePage: NextPage = () => {
   const [schedules, setSchedules] = useState<Schedule[] | null>([]);

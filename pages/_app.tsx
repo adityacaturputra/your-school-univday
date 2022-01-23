b490e1478087603b603b875d5af7029b5450b97c
adityacaturputra <adityacaturputra25@gmail.com>
2022-01-23 14:46:21 +0700

    feature: add pwa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,22 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 import Navbar from '../src/components/molecules/NavBar';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   
   return (    
-    <div className="h-screen bg-gradient-to-r from-gray-100 to-gray-50 bg-center bg-cover">
-      <div className={'h-screen flex flex-col justify-center'}>
-        <Navbar/>
-        <Component {...pageProps} />;
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <div className="h-screen bg-gradient-to-r from-gray-100 to-gray-50 bg-center bg-cover">
+        <div className={'h-screen flex flex-col justify-center'}>
+          <Navbar/>
+          <Component {...pageProps} />;
+        </div>
       </div>
-    </div>
+    </>
   );
-
-
-
 }
 export default MyApp;

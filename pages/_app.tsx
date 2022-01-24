715ddff40adbc9a15bbd760ef1a1fd1d1b302145
adityacaturputra <adityacaturputra25@gmail.com>
2022-01-24 09:23:27 +0700

    style: add nprogress when move to other page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,40 @@
 import '../styles/globals.css';
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Navbar from '../src/components/molecules/NavBar';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+const NProgress = require('nprogress');
+import 'nprogress/nprogress.css';
+import '../styles/nprogress.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChangeOnStart = (url:any, { shallow }:any) => {
+      NProgress.start();
+    };
+    const handleRouteChangeOnComplete = (url:any, { shallow }:any) => {
+      NProgress.done();
+    };
+    const handleRouteChangeOnError = (url:any, { shallow }:any) => {
+      NProgress.done();
+    };
+
+    router.events.on('routeChangeStart', handleRouteChangeOnStart);
+    router.events.on('routeChangeComplete', handleRouteChangeOnComplete);
+    router.events.on('routeChangeError', handleRouteChangeOnError);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method:
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeOnStart);
+      router.events.off('routeChangeComplete', handleRouteChangeOnComplete);
+      router.events.off('routeChangeError', handleRouteChangeOnError);
+    };
+  }, []);
+
   return (    
     <>
       <Head>

d3b7b9c592a0c4a7eb107a9cc9ffd55ae1765e01
adityacaturputra <adityacaturputra25@gmail.com>
2022-01-25 11:28:32 +0700

    disable pwa

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa');
+// const withPWA = require('next-pwa');
 
-module.exports = withPWA({
-  reactStrictMode: true,
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-  },
-});
+module.exports = {
+  reactStrictMode: true
+};
+
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   pwa: {
+//     dest: 'public',
+//     register: true,
+//     skipWaiting: true,
+//   },
+// });
